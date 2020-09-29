b0120a5b4fea4fcf2072a0674a20e4f70ca70b81
Neha Gupta <neha.gupta453@webkul.com>
2020-09-29 20:04:00 +0530

    FIX export/ import data issue

--- image_in_excel/controllers/controllers.py
+++ image_in_excel/controllers/controllers.py
@@ -19,19 +19,16 @@ from odoo.http import request
 from odoo.exceptions import UserError
 from odoo.addons.web.controllers.main import ExcelExport,serialize_exception,ExportFormat
 
-import logging
-_logger = logging.getLogger(__name__)
-
 try:
-    import xlsxwriter
-except Exception as e:
-    _logger.info("Exception Found in PyJWT External Library : %r .", str(e))
+    from odoo.tools.misc import xlsxwriter
+except ImportError:
+    xlsxwriter = None
 
 class ExcelExport_New(ExcelExport):
     # Excel needs raw data to correctly handle numbers and date values
     raw_data = True
 
-    @http.route('/web/export/xls', type='http', auth="user")
+    @http.route('/web/export/xlsx', type='http', auth="user")
     @serialize_exception
     def index(self, data, token):
         return self.base(data, token)
@@ -41,7 +38,7 @@ class ExcelExport_New(ExcelExport):
         return 'application/vnd.ms-excel'
 
     def filename(self, base):
-        return base + '.xls'
+        return base + '.xlsx'
 
     # ===================================================================================================
     #                        few functions use to store images and generate file name
@@ -57,7 +54,6 @@ class ExcelExport_New(ExcelExport):
         return path+"/"+str(row_index)+str(cell_index)
 
     def _create_image(self,path,data,format):
-        _logger.info("_____________BABABBBABBABBAB______%r",path+"."+format)
         f = open(path+"."+format,"wb")
         f.write(base64.b64decode(data))
         f.close()
@@ -69,9 +65,8 @@ class ExcelExport_New(ExcelExport):
 
         path=self._full_folder_path("WK_export_update") # ================ Use to Create Folder to store File
 
-        workbook = xlsxwriter.Workbook(path+"/file_data.xls") # ============= Use xlsxwriter instead of xlwt
+        workbook = xlsxwriter.Workbook(path+"/file_data.xlsx") # ============= Use xlsxwriter instead of xlwt
         worksheet = workbook.add_worksheet('Sheet 1')
-        _logger.info("Hey!! Path Where Data is Stored Is:%r",path)
         # ===================================================================================================
         #                    check that the folder exsists or not if not then create it
         # ===================================================================================================
@@ -100,36 +95,19 @@ class ExcelExport_New(ExcelExport):
                         #                               lde to store image not as b64
                         # ======================================================================================
                         mimetype = guess_mimetype(base64.b64decode(cell_value))
-
                         path1=self._generate_semi_path(path, row_index, cell_index)
-                        _logger.info("________________BABA_________________%r",path1)
                         if (mimetype.startswith('image')):
-                            cell_value = odoo.tools.image_resize_image(base64_source=cell_value, size=(100 or None, 100 or None),encoding='base64',filetype='PNG')
-                            if mimetype == 'image/jpeg':
-                                self._create_image(path1,cell_value,"jpeg")
-                                worksheet.insert_image(row_index + 1, cell_index, path1+".jpeg", options={})
-                                continue
-                            elif mimetype == 'image/png':
-                                self._create_image(path1,cell_value,"png")
-                                worksheet.insert_image(row_index + 1, cell_index, path1+".png", options={})
-                                continue
-                            elif mimetype == 'image/bmp':
-                                self._create_image(path1,cell_value,"bmp")
-                                worksheet.insert_image(row_index + 1, cell_index, path1+".bmp", options={})
-                                continue
-                            elif mimetype == 'image/gif':
-                                self._create_image(path1,cell_value,"gif")
-                                worksheet.insert_image(row_index + 1, cell_index, path1+".gif", options={})
-                                continue
-                            else:
-                                cell_value = pycompat.to_text(cell_value)
-                            # =================================================================================
+                            cell_value = odoo.tools.image_process(cell_value, size=odoo.tools.image_guess_size_from_field_name(fieldname), crop=False, quality=0)
+                            img_format = mimetype.split('/')[1]
+                            self._create_image(path1,cell_value,img_format)
+                            worksheet.insert_image(row_index + 1, cell_index, path1+"."+img_format, options={})
                         else:
                             cell_value = pycompat.to_text(cell_value)
                     except UnicodeDecodeError:
                         raise UserError(_("Binary fields can not be exported to Excel unless their content is base64-encoded. That does not seem to be the case for %s.") % fields[cell_index])
 
-                if isinstance(cell_value, pycompat.string_types):
+                # if isinstance(cell_value, pycompat.string_types):
+                if isinstance(cell_value, bytes):
                     cell_value = re.sub("\r", " ", pycompat.to_text(cell_value))
                     # Excel supports a maximum of 32767 characters in each cell:
                     cell_value = cell_value[:32767]
@@ -143,7 +121,7 @@ class ExcelExport_New(ExcelExport):
         #                    Store the data in xls and then open and send that file to the user
         # =======================================================================================================
         workbook.close()
-        fp = open(path+"/file_data.xls","rb")
+        fp = open(path+"/file_data.xlsx","rb")
         # =======================================================================================================
         data = fp.read()
         fp.close()
