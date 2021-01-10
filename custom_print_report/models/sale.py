da10fe9923586cac31f3de9121add6cd9faf7851
evo-mk <markus.kaltenbrunner@evo-tech.eu>
2021-01-10 13:59:32 +0100

    update uploadsize

--- custom_print_report/models/sale.py
+++ custom_print_report/models/sale.py
@@ -21,6 +21,7 @@
 #
 ##############################################################################
 
+from odoo.tools.misc import get_lang
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
@@ -37,6 +38,49 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     is_printing_inv = fields.Boolean(string='Is Printing Invoice?')
+
+    def action_invoice_sent(self):
+        """ Open a window to compose an email, with the edi invoice template
+            message loaded by default
+        """
+        self.ensure_one()
+        template = self.env.ref('account.email_template_edi_invoice', raise_if_not_found=False)
+        if self.is_printing_inv:
+            template_rec = self.env['email.template'].sudo().search([('name', 'ilike', '3D-Printing-Service-Invoice: Send by email')], limit=1)
+            if template_rec:
+                template = template_rec
+        lang = get_lang(self.env)
+        if template and template.lang:
+            lang = template._render_template(template.lang, 'account.move', self.id)
+        else:
+            lang = lang.code
+        compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
+        ctx = dict(
+            default_model='account.move',
+            default_res_id=self.id,
+            # For the sake of consistency we need a default_res_model if
+            # default_res_id is set. Not renaming default_model as it can
+            # create many side-effects.
+            default_res_model='account.move',
+            default_use_template=bool(template),
+            default_template_id=template and template.id or False,
+            default_composition_mode='comment',
+            mark_invoice_as_sent=True,
+            custom_layout="mail.mail_notification_paynow",
+            model_description=self.with_context(lang=lang).type_name,
+            force_email=True
+        )
+        return {
+            'name': _('Send Invoice'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'account.invoice.send',
+            'views': [(compose_form.id, 'form')],
+            'view_id': compose_form.id,
+            'target': 'new',
+            'context': ctx,
+        }
 
 
 class SaleOrder(models.Model):
@@ -58,7 +102,7 @@ class SaleOrder(models.Model):
             if self.is_printing_inv:
                 template = self.env['mail.template'].search([('name', '=', 'Sales Order 3D Printing Service')], limit=1)
                 if template:
-                    template_id = template
+                    template_id = template.id
 
         return template_id
 
