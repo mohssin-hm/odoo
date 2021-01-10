65de3b7e1f24c047551d2e9cf455f435c31080f3
evo-mk <markus.kaltenbrunner@evo-tech.eu>
2021-01-10 15:42:46 +0100

    update uploadsize

--- website_elearning_video/wizard/attachment_wizard.py
+++ website_elearning_video/wizard/attachment_wizard.py
@@ -23,7 +23,7 @@ class VideoAttachmentWizard(models.TransientModel):
     _name = "video.attachment.wizard"
 
     attachment = fields.Binary(
-        string="Attachment", 
+        string="Attachment",
         required=True)
     name = fields.Char(
         string='Name')
@@ -32,31 +32,35 @@ class VideoAttachmentWizard(models.TransientModel):
     def add_video_attachment(self):
         modelName = self._context.get('active_model')
         modelId = self._context.get('active_id')
-  
+
         attachmentValue = {
-                        'name': self.name,
-                        'datas': self.attachment,
-                        'res_model': modelName,
-                        'res_id': modelId,
-                        'type': 'binary',
-                        'db_datas': self.name,
-                        'res_name': self.name,
-                    }
+            'name': self.name,
+            'datas': self.attachment,
+            'res_model': modelName,
+            'res_id': modelId,
+            'type': 'binary',
+            'db_datas': self.name,
+            'res_name': self.name,
+        }
         res = self.env['ir.attachment'].create(attachmentValue)
-        file_size = 25 * 1024 * 1024
+        file_size = 500 * 1024 * 1024
 
-        allowed_types = self.env['website.slide.video'].search([('is_active','=',True)],limit=1)
+        allowed_types = self.env['website.slide.video'].search(
+            [('is_active', '=', True)], limit=1)
         allowed_format = []
+        flag = False
         for format in allowed_types.allowed_types:
-            allowed_format.append(format.name) 
-
-        if (res.mimetype in allowed_format and res.file_size <= file_size):
+            allowed_format.append(format.name)
+        if allowed_format:
+            if res.mimetype not in allowed_format:
+                flag = True
+        if (not flag and res.file_size <= file_size):
             attachmentObj = self.env['slide.slide'].browse(modelId)
             attachmentObj.slide_attachment = res.id
-        
+
         else:
             res.unlink()
-            raise UserError(_("Only allowed formats and size less than 25Mb can be uploaded."))
-            
+            raise UserError(
+                _("Only allowed formats and size less than 25Mb can be uploaded."))
 
         return True
